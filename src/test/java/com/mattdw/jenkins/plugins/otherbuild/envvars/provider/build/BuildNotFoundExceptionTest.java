a1b6f95d1bd03545e271ee43abedd22777359c2f
M.D.Ward <matthew.ward@byng.co>
2016-03-22 12:02:00 +0000

    Added tests for "not found" exceptions.

---
+++ src/test/java/com/mattdw/jenkins/plugins/otherbuild/envvars/provider/build/BuildNotFoundExceptionTest.java
@@ -0,0 +1,55 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 M.D.Ward <matthew.ward@byng.co>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.mattdw.jenkins.plugins.otherbuild.envvars.provider.build;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author M.D.Ward <matthew.ward@byng.co>
+ */
+public class BuildNotFoundExceptionTest {
+    
+    private final String project = "PROJECT NAME";
+    private final String buildId = "BUILD IDENTIFIER";
+    private BuildNotFoundException exception;
+        
+    @Before
+    public void setUp() {
+        this.exception = new BuildNotFoundException(this.project, this.buildId);
+    }
+
+    @Test
+    public void testGetProject() {
+        assertSame(this.project, this.exception.getProject());
+    }
+
+    @Test
+    public void testGetId() {
+        assertSame(this.buildId, this.exception.getId());
+    }
+    
+}
